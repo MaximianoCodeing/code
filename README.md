98788356e2aa65bf87b237ad5fdaffed536d648d
James Aprosail <aprosail@outlook.com>
2025-09-06 16:25:41 +0800

    chore: setup next app

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Vibe Specifier
+# Vibe Specifier Code
 
-The VibeSpecifier application for codebase development with version control and Issue&amp;PR model.
+The VibeSpecifier application for codebase development with version control and Issue&amp;PR model: Follow your vibe ideas and let the application do the rest.
---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next"
+import { ReactNode } from "react"
+
+export const metadata: Metadata = {
+  title: "VibeSpecifier Code",
+  description: "Follow your vibe and let application do the rest.",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
